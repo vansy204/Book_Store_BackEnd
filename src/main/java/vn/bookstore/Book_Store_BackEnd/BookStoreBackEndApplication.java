5ae9e059d5c2b7716bd09e8e79c882c86d738a1d
VanSy <phamvansy204@gmail.com>
2024-07-22 23:26:39 +0700

    Khởi tạo dự án

---
+++ src/main/java/vn/bookstore/Book_Store_BackEnd/BookStoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package vn.bookstore.Book_Store_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/bookstore/Book_Store_BackEnd/BookStoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.bookstore.Book_Store_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
