75aa7be2cdf53ed55dfb3aa09ce88088512e8748
VanSy <phamvansy204@gmail.com>
2024-07-26 23:24:46 +0700

    update searching

--- src/main/java/vn/bookstore/Book_Store_BackEnd/dao/BookRepository.java
+++ src/main/java/vn/bookstore/Book_Store_BackEnd/dao/BookRepository.java
@@ -1,9 +1,13 @@
 package vn.bookstore.Book_Store_BackEnd.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 import vn.bookstore.Book_Store_BackEnd.entity.Book;
 @RepositoryRestResource(path = "book")
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Page<Book> findByBookNameContaining(@RequestParam("bookName") String bookName, Pageable pageable);
 }
